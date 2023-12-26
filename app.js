f3dccd24265c38154ab97ad8105a67b736fa1a82
SubuCK <subuck@pentafox.in>
2023-12-26 22:09:54 +0530

    igniting our app

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const nested_divs = React.createElement(
   "div",
   { id: "parent" },
   [React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "This is h1 tag"),
-    React.createElement("h2", { id: "heading" }, "This is h2 tag"),
+    React.createElement("h1", { id: "heading" }, "This is vanakkam react 🚀"),
+    React.createElement("h2", { id: "heading" }, "This is Subbu"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "This is h1 tag"),
+    React.createElement("h1", { id: "heading" }, "This is heading tag"),
     React.createElement("h2", { id: "heading" }, "This is h2 tag"),
   ])]
 );
--- readme.md
+++ readme.md
@@ -1,1 +1,18 @@
 # Vanakkam react 🚀
+
+# Parcel
+- Dev build
+- Local server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistent hashing
+- Code Splitting
+- Differential Bundling - support older browser
+- Diagnostics
+- HTTPs
+- Tree shaking : remove unused code
+- Different dev and prod bundles
